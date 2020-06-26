7f6ca096f857bcf216388a6338f34d5b5633655d
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-26 13:30:34 -0400

    Updating username & password.

--- terraform/kinesis_data_generator/cloudformation.tf
+++ terraform/kinesis_data_generator/cloudformation.tf
@@ -1,10 +1,12 @@
 data "aws_region" "current" {}
 
-resource "random_string" "this" {
+resource "random_string" "username" {
   length = 16
+  special = false
+  number = false
 }
 
-resource "random_password" "this" {
+resource "random_string" "password" {
   length = 16
   special = false
   min_numeric = 1
@@ -14,8 +16,8 @@ resource "aws_cloudformation_stack" "this" {
   name = "Kinesis-Data-Generator-Cognito-User"
   template_url = "https://aws-kdg-tools.s3.${data.aws_region.current.name}.amazonaws.com/cognito-setup.json"
   parameters = {
-    Username = random_string.this.result
-    Password = random_password.this.result
+    Username = random_string.username.result
+    Password = random_string.password.result
   }
   capabilities = ["CAPABILITY_IAM"]
 }
