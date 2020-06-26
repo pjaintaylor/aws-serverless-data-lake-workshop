31a57cf7d1f92a6fdd9170295a215febc45892d5
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-26 13:31:26 -0400

    Updating outputs.

--- terraform/kinesis_data_generator/outputs.tf
+++ terraform/kinesis_data_generator/outputs.tf
@@ -2,8 +2,8 @@ output "kinesis_data_generator_url" {
   value = aws_cloudformation_stack.this.outputs["KinesisDataGeneratorUrl"]
 }
 output "username" {
-  value = random_string.this.result
+  value = random_string.username.result
 }
 output "password" {
-  value = random_password.this.result
+  value = random_string.password.result
 }
