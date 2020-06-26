dee3423a6f41ba68af2025a48594f905a5ebeef1
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-26 13:05:08 -0400

    Removing lambda function.

--- terraform/kinesis_data_generator/lambda_function.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_lambda_function" "this" {
-  description = "Creates a Cognito User Pool, Identity Pool, and a User.  Returns IDs to be used in the Kinesis Data Generator."
-  function_name = "KinesisDataGeneratorCognitoSetup"
-  handler = "createCognitoPool.createPoolAndUser"
-  role = aws_iam_role.lambda_execution_role.arn
-  runtime = "nodejs12.x"
-  s3_bucket = "aws-kdg-tools-${data.aws_region.current.name}"
-  s3_key = "datagen-cognito-setup.zip"
-}
--- terraform/kinesis_data_generator/outputs.tf
+++ terraform/kinesis_data_generator/outputs.tf
@@ -1,6 +1,6 @@
-output "kinesis_data_generator_url" {
+/*output "kinesis_data_generator_url" {
   value = "https://awslabs.github.io/amazon-kinesis-data-generator/web/producer.html?${jsondecode(data.aws_lambda_invocation.this.result)["Querystring"]}"
-}
+}*/
 output "username" {
   value = random_string.this.result
 }
